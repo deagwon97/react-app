84fa01e4b8e22de0503fcc7366fe95a2ec08730a
curiosity806 <46279272+curiosity806@users.noreply.github.com>
2021-02-28 15:20:43 +0900

    update

--- src/App.js
+++ src/App.js
@@ -6,15 +6,63 @@ import Subject from "./components/Subject";
 
 
 class App extends Component {
-  render() {
+  constructor (props){
+    super(props);
+    this.state = {
+      mode : "welcome",
+      subject : {title:"React", sub:"Hello react world!"},
+      welcom : {title:"Hello!", desc:"Welcome to the react world"},
+      contents : [
+        {id:1, title:"HTML and CSS", desc:"HTML CSS is HyperText Markup Language"},
+        {id:2, title:"HTML", desc:"HTML is HyperText Markup Language"},
+        {id:3, title:" CSS", desc:"CSS is HyperText Markup Language"},
+        {id:4, title:"Js", desc:"JS is HyperText Markup Language"}
+      ]
+      }
+    }
+
+    render() {
+      console.log("App render")
+      var _title, _desc = null;
+      if(this.state.mode === "welcome"){
+        _title = this.state.welcom.title;
+        _desc = this.state.welcom.desc;
+      }
+      else if(this.state.mode === "read"){
+        _title = this.state.contents[0].title;
+        _desc = this.state.contents[0].desc;
+      }
       return (
       <div className="App">
-        <Subject title = "React" sub="for UI"></Subject>
-        <TOC></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language"></Content>
+
+        {/* <Subject
+         title = {this.state.subject.title}
+         sub = {this.state.subject.sub}
+         ></Subject> */}
+
+         
+        <header>
+          <h1><a href = "/" onClick={function(e){
+            console.log(e);
+            e.preventDefault();
+            //this.state.mode = "welcome";
+            //debugger;
+            alert("Hi")
+          }}>{this.state.subject.title}</a></h1>
+          {this.state.subject.sub}
+        </header>
+
+
+        <TOC data = {this.state.contents}></TOC>
+        <Content title={_title}
+                 desc={_desc}
+                 ></Content>
       </div>
-    );
-  }
+      );
+    }
 }
+
+
+
 
 export default App;
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -3,12 +3,23 @@ import { Component } from 'react';
 
 class TOC extends Component{
   render() {
+    console.log("TOC render")
+    var lists = [];
+    var data = this.props.data;
+    var i = 0;
+
+    while(i < data.length){
+      // data는 상위 components에서 전달 받은 props
+      // while문을 이용
+      // props를 가공하여 li 태그를 생성
+      lists.push(<li key = {data[i].id} ><a href={"/content/"+data[i].id}>{data[i].title}</a></li>)
+      i = i + 1;
+    }
+
     return (
       <nav>
         <ul>
-          <li><a href="1.html">HTML</a></li>
-          <li><a href="2.html">CSS</a></li>
-          <li><a href="3.html">JavaScript</a></li>
+          {lists}
         </ul>
       </nav>
     );
