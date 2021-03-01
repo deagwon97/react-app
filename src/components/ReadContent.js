99c72a94a84e93edbe71613cbbb4d1e00d374b28
curiosity806 <46279272+curiosity806@users.noreply.github.com>
2021-03-01 21:29:24 +0900

    update

---
+++ src/components/ReadContent.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+
+class ReadContent extends Component {
+  render() {
+    return (
+      <article>
+        <h2>Read</h2>
+        <form action=""></form>
+      </article>
+    );
+  }
+}
+
+export default ReadContent;
